f83b78e41a10d8bfb4db296e2e6442a859befd17
ToastyyX <erick.cool321@icloud.com>
2023-07-13 11:42:04 -0500

    Initial commit: Added Project

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    private int data; // The element gets stored in this node
+    private Node link; // 'link' is a reference to the next node
+    public void random() {
+        System.out.println("Hello world!");
+        System.out.println("New Project!");
+    }
+
+
+}
