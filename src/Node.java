4f37f2efbff166bd93e3b4a4b06f628ac4b0db67
ToastyyX <erick.cool321@icloud.com>
2023-07-13 20:04:07 -0500

    3rd Commit: Refined Documentation

--- src/Node.java
+++ src/Node.java
@@ -2,28 +2,28 @@ public class Node {
     private int data; // 'data' is the value stored in the node
     private Node link; // 'link' is a reference to the next node
 
-    public Node(int initialData, Node initialLink) { // Constructor
+    public Node(int initialData, Node initialLink) { // Constructor for the Node class
         data = initialData;
         link = initialLink;
     }
 
-    public Node getLink() {
+    public Node getLink() { // Returns the link to the next node
         return link;
     }
 
-    public int getData() {
+    public int getData() { // Returns the data stored in the node
         return data;
     }
 
-    public void setData(int newData) { // 'newData' is the new data to place in a node
+    public void setData(int newData) { // Sets the data stored in the node
         data = newData;
     }
 
-    public void setLink(Node newLink) { // 'newLink' is a reference to the next node or NPE if no node is present
+    public void setLink(Node newLink) { // Sets the link to the next node
         link = newLink;
     }
 
-    public void addNodeAfter(int element) throws OutOfMemoryError {
+    public void addNodeAfter(int element) throws OutOfMemoryError { // Adds a node after the current node
         link = new Node(element, link);
     }
 
