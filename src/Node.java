701dd84f9085b1f14b46e3802f098431ebc1dd3f
ToastyyX <erick.cool321@icloud.com>
2023-07-18 23:15:56 -0500

    4th Commit: Additional Methods for 'Node' Class

--- src/Node.java
+++ src/Node.java
@@ -11,6 +11,33 @@ public class Node {
         return link;
     }
 
+    public static Node listSearch(Node head, int target) { // Search for a particular piece of data in a linked list
+        Node cursor;
+        for (cursor = head; cursor != null; cursor = cursor.link) {
+            if (target == cursor.data) {
+                return cursor;
+            }
+        }
+        return null;
+    }
+
+    public static void listData(Node head) {
+        Node cursor;
+        System.out.println("Linked List:");
+        for (cursor = head; cursor != null; cursor = cursor.link) {
+            System.out.println(cursor.data);
+        }
+    }
+
+    public static int listLength(Node head) {
+        int answer = 0;
+        Node cursor;
+        for (cursor = head; cursor != null; cursor = cursor.link) {
+            answer++;
+        }
+        return answer;
+    }
+
     public int getData() { // Returns the data stored in the node
         return data;
     }
@@ -27,4 +54,7 @@ public class Node {
         link = new Node(element, link);
     }
 
+    public void removeNodeAfter() { // Removes the node after the current node
+        link = link.link;
+    }
 }
